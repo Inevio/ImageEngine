052345cf5af5e941c8da809d234510ca265e2e96
Elena Mengual Mesa <menme@alumni.uv.es>
2017-02-13 16:07:14 +0100

    Dos tipos de zoom, dependiendo del tamaño

--- programs.js
+++ programs.js
@@ -14,8 +14,8 @@
      var up;
      var down;
 
-     var tam_view_x = 350; // Tamaño de la cámara
-     var tam_view_y = 350; // Tamaño de la cámara
+     var tam_view_x = 250; // Tamaño de la cámara
+     var tam_view_y = 250; // Tamaño de la cámara
 
      var canvas_destino_x = tam_view_x;
      var canvas_destino_y = tam_view_y;
@@ -25,13 +25,17 @@
      var tam_x_img;
      var tam_y_img;
 
+     var tam_x_false;
+     var tam_y_false;
+
+     var profundidad = false;
      //var cuadros; // Cuadros máximos para cargar la ampliación
 /*
      var sx = 0;
      var sy = 0;
      var sw;
      var sh;*/
-
+     var proporcion_easy_x;
      var dx;
      var dy;
      var dw;
@@ -41,7 +45,13 @@
      grande.src = "cat/big.jpg";*/
      function divide (){
        tam_X = (reduc.width)/cols;
-       tam_Y = (reduc.height)/fil;}
+       tam_Y = (reduc.height)/fil;
+
+       tam_x_false = tam_X*2;
+       tam_y_false = tam_Y*2;
+
+       proporcion_easy_x = tam_x_false / tam_X;
+     }
 
      /* Calcular el numero de cols y filas que son necesarias
      para cargar la imagen */
@@ -70,9 +80,18 @@
          indices = [];
          getMouseLocationX(event); // Coordenadas en el canvas del ratón en x
          getMouseLocationY(event); // Coordenadas en el canvas del ratón en y
-         position();               // Calcula en qué cuadrante del canvas se encuentra
-         calcular_vecinos();       // Calcula las imágenes necesarias para ampliar la zona y las carga
-         display();              // Dibujar la imagen final
+         eleccion()
+       }
+       function eleccion (){
+         //console.log(proporcion_easy_x);
+         if (profundidad){
+           position();               // Calcula en qué cuadrante del canvas se encuentra
+           calcular_vecinos();       // Calcula las imágenes necesarias para ampliar la zona y las carga
+           display();                // Dibujar la imagen final
+        }
+        else {
+          displayEasy();
+        }
        }
 
        /*Devuelve la coordenada X del ratón */
@@ -91,14 +110,11 @@
        /* Calcular posición actual */
        function position (){
          numero_actual = (parseInt(cursor_x / tam_X ) + parseInt(cursor_y / tam_Y ) * cols);
-         //console.log(numero_actual, cursor_x,tam_X,cursor_y,tam_Y,parseInt(cursor_x / tam_X ) ,parseInt(cursor_y / tam_Y ));
-         console.log(numero_actual);/*
-         if ( numero_actual >= cols * fil ){
-           numero_actual = 0;
-           cursor_x = 0;
-           cursor_y = 0;
-           //console.log("Error al seleccionar cuadro");
-         }*/
+         proporcion_easy_x = tam_x_false / tam_X;
+         if (proporcion_easy_x < 1){
+           profundidad = false;
+           eleccion();
+           return;}
        }
 
        function calcular_vecinos(){
@@ -140,13 +156,15 @@
        function display(){
          var c = document.getElementById("muestra");
          var ctx = c.getContext("2d");
+         //console.log("-----------------------");
+         //console.log("pedacitos");
 
+         relacion_x = tam_x_img / tam_X;
+         relacion_y = tam_y_img / tam_Y;
 
-         relacion_x = (cols * tam_x_img) / (cols * tam_X);
-         relacion_y = (fil * tam_y_img) / (fil * tam_Y);
+         desplazamiento_x = (0.5 * canvas_destino_x) - cursor_x * relacion_x;
+         desplazamiento_y = (0.5 * canvas_destino_y) - cursor_y * relacion_y;
 
-         desplazamiento_x =  (0.5 * canvas_destino_x) - cursor_x*relacion_x ;
-         desplazamiento_y = (0.5 * canvas_destino_y) - cursor_y*relacion_y ;
 
          ctx.fillStyle = "#FFFFFF";
          ctx.fillRect(0,0,canvas_destino_x,canvas_destino_y);
@@ -158,40 +176,26 @@
 
          })
        }
-var minimo = 0;
-var contador = 3;
+//var minimo = 0;
+//var contador = 3;
        function zoom(alpha){
-         if (alpha > 0  || contador > minimo){
-           maximo();
+         //if (alpha > 0  || contador > minimo){
            indices=[];
-           calcular_vecinos();
-
+           //var beta = 50;
          //tam_view_x -= alpha * 100;
          //tam_view_y -= alpha * 100 * proporcion;
-         tam_y_img += alpha * 100 * tam_y_img/tam_x_img;
-         tam_x_img += alpha * 100;
-         contador += alpha;
+         tam_x_false += alpha * 50 * tam_x_img/tam_y_img;
+         tam_y_false += alpha * 50;
+
+         tam_x_img = tam_x_false;
+         tam_y_img = tam_y_false;
+
+         //contador += alpha;
+       //}
+         maximo();
+         eleccion();
        }
 
-         display();
-       }
-/*       function zoom_out(){
-         tam_view_x += 100;
-         tam_view_y += 100 * proporcion;
-         tam_y_img -= 100 * tam_y_img/tam_x_img;
-         tam_x_img -= 100
-       }
-
-       $function(){
-         $('#visor').html(navigator.visor);
-         function (event, delta){
-           if (delta > 0)
-            zoom_in();
-           else
-            zoom_out();
-        }
-      }
-      */
       function Scroll(){
       var situacion = document.getElementById("visor");
       if (situacion.addEventListener){
@@ -208,4 +212,34 @@ var contador = 3;
         var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
         zoom(delta);
         return false;
+      }
+
+      function displayEasy(){
+        //console.log("easy");
+        var c = document.getElementById("muestra");
+        var ctx = c.getContext("2d");
+
+        var reduc = new Image();
+        reduc.src = "mariposa/reduc.jpeg";
+
+        proporcion_easy_x = tam_x_false / tam_X;
+        if (proporcion_easy_x > 1){
+          profundidad = true;
+          eleccion();
+          return;}
+        var proporcion_easy_y = tam_y_false / tam_Y;
+
+        desplazamiento_x =  -cursor_x * proporcion_easy_x + (0.5 * canvas_destino_x);
+        desplazamiento_y =  -cursor_y * proporcion_easy_y + (0.5 * canvas_destino_y) ;
+        //console.log(desplazamiento_x, desplazamiento_y);
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillRect(0,0,canvas_destino_x,canvas_destino_y);/*
+        ctx.fillStyle = "#FF0000";
+        ctx.beginPath();
+        ctx.arc(desplazamiento_x, desplazamiento_y, 4, 0, 3.1415*4);
+        ctx.stroke();
+        ctx.closePath();*/
+        //dx = (((item % cols ) * tam_X  ) * relacion_x ) + desplazamiento_x;
+        //dy = ((((item - (item % cols )) / cols ) * tam_Y ) * relacion_y) + desplazamiento_y;
+        ctx.drawImage(reduc, desplazamiento_x, desplazamiento_y, tam_x_false*cols, tam_y_false*fil);
       }
