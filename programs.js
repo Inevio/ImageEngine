7efa4ad9d88ba66b6997af5850ddf1a8444064e8
Elena Mengual Mesa <menme@alumni.uv.es>
2017-02-27 08:40:11 +0100

    Limit top and left

--- programs.js
+++ programs.js
@@ -1,243 +1,323 @@
-     var column = 6;
-     var row = 4;
-     var k = 0;
-     var urls = [];
-     var img = [];
-     var index = [];
-     var where_you_are = 0;
+var column = 6;
+var row = 4;
+var k = 0;
+var urls = [];
+var img = [];
+var index = [];
+var where_you_are = 0;
+//var cursor_x;
+//var cursor_y;
+var last_point= [];
+var right;
+var left;
+var up;
+var down;
 
-     //var cursor_x;
-     //var cursor_y;
+var flag = false;
 
-     var right;
-     var left;
-     var up;
-     var down;
+var size_view_x = 250;
+var size_view_y = 250;
 
-     var flag = false;
+var last_mouse=[];
 
-     var size_view_x = 250; // Tamaño de la cámara
-     var size_view_y = 250; // Tamaño de la cámara
+var canvas_destination_x;
+var canvas_destination_y;
 
-     var canvas_destination_x = size_view_x;
-     var canvas_destination_y = size_view_y;
+var one = true;
 
-     //var proporcion = canvas_destination_x/canvas_destination_y;
+var size_x_img;
+var size_y_img;
 
-     var size_x_img;
-     var size_y_img;
+var size_x_false;
+var size_y_false;
 
-     var size_x_false;
-     var size_y_false;
+var depth = false;
+var proportion_easy_x;
+var dx;
+var dy;
+var dw;
+var dh;
+var point_x = 0;
+var point_y = 0;
 
-     var depth = false;
-     //var cuadros; // Cuadros máximos para cargar la ampliación
-/*
-     var sx = 0;
-     var sy = 0;
-     var sw;
-     var sh;*/
-     var proportion_easy_x;
-     var dx;
-     var dy;
-     var dw;
-     var dh;
-     /*
-     grande = new Image();
-     grande.src = "mariposa/big.jpeg";*/
-     function divide (){
-       size_x = (reduc.width)/column;
-       size_y = (reduc.height)/row;
+var desplazamiento_x;
+var desplazamiento_y;
 
-       size_view_x = size_x * column;
-       size_view_y = size_y * row;
+function first(){
+  point_x = canvas_destination_x/2;
+  ////console.log(point_x);
+  point_y = canvas_destination_y/2;
+  ////console.log(point_y);
+  last_mouse[0] = point_x;
+  last_mouse[1] = point_y;
 
-       size_x_false = size_x*2;
-       size_y_false = size_y*2;
+  desplazamiento_x = (0.5 * canvas_destination_x) - point_x * relacion_x ;
+  desplazamiento_y = (0.5 * canvas_destination_y) - point_y * relacion_y ;
 
-       proportion_easy_x = size_x_false / size_x;
+  position();
+  calculate_neighbour();
+  display();
+  one = false;
+  //console.log(move_x);
+}
+
+function save(){
+  last_mouse[0] = cursor_x;
+  last_mouse[1] = cursor_y;
+
+  last_point[0] = point_x;
+  last_point[1] = point_y;
+}
+
+function rest(){
+  move_x = cursor_x - last_mouse[0];
+  move_y = cursor_y - last_mouse[1];
+  if (move_x > 10 || move_x < -10)
+    move_x=move_x%10;
+  if (move_y > 10 || move_y < -10)
+    move_y=move_y%10;
+
+  //console.log(move_x);
+  }
+
+function divide (){
+  size_x = (reduc.width)/column;
+  size_y = (reduc.height)/row;
+
+  size_view_x = size_x * column;
+  size_view_y = size_y * row;
+
+  size_x_false = size_x*2;
+  size_y_false = size_y*2;
+
+  proportion_easy_x = size_x_false / size_x;
+}
+
+function maximum (){
+  max_carga_ancho = column;
+  max_carga_alto  = row;
+  if (size_x_img < canvas_destination_x)
+   max_carga_ancho = column + 1;
+  if (size_y_img < canvas_destination_y)
+   max_carga_alto = row + 1;
+
+   relacion_x = size_x_img/size_x;
+   relacion_y = size_y_img/size_y;
+}
+
+function table (){
+  divide();
+ for ( j = 0; j < column*row; j++)
+   urls[j] = "mariposa/" + j + ".jpeg";
+  }
+
+  function getMouseLocationX(event) {
+    var coordenadas = visor.getBoundingClientRect();
+    cursor_x = event.pageX - coordenadas.left;
+  }
+
+  function getMouseLocationY(event){
+    var coordenadas = visor.getBoundingClientRect();
+    cursor_y = event.pageY - coordenadas.top;
+  }
+
+  function invento(){
+    point_x += last_mouse[0] - cursor_x;
+    point_y += last_mouse[0] - cursor_y;
+  }
+
+  function zoom(alpha){
+    point_x = cursor_x;
+    point_y = cursor_y;
+    index=[];
+    size_x_false += alpha * 20 * size_x_img/size_y_img;
+    size_y_false += alpha * 20;
+
+    size_x_img = size_x_false;
+    size_y_img = size_y_false;
+
+    maximum();
+    election();
+  }
+
+ function Scroll(){
+ var situacion = document.getElementById("visor");
+ if (situacion.addEventListener){
+   //IE9, Chrome, Safari, Opera
+   situacion.addEventListener("mousewheel", MouseWheelHandler, false);
+   //Firefox
+   situacion.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+ }
+ //IE 6/7/8
+ else situacion.attachEvent("onmousewheel", MouseWheelHandler);
+}
+
+ function MouseWheelHandler(e){
+   var e = window.event || e; //IE
+   var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+   zoom(delta);
+   return false;
+ }
+
+ function displayEasy(){
+   var c = document.getElementById("visor");
+   var ctx = c.getContext("2d");
+
+   var reduc = new Image();
+   reduc.src = "mariposa/reduc.jpeg";
+
+   if (proportion_easy_x > 1){
+     depth = true;
+     election();
+     return;}
+
+   var proporcion_easy_y = size_y_false / size_y;
+
+   ctx.fillStyle = "#FFFFFF";
+   ctx.fillRect(0,0,size_x * column, size_y * row);
+
+   ctx.drawImage(reduc, point_x-size_x_false*column/2, point_y-size_y_false*row/2);
+ }
+
+ function mousepress(event){
+   if(flag){
+   getMouseLocationX(event);
+   getMouseLocationY(event);
+
+   last_mouse_x = cursor_x;
+   last_mouse_y = cursor_y;
+   //console.log("BYE!!  " + move_x );
+  }
+   if(!flag){
+     getMouseLocationX(event);
+     getMouseLocationY(event);
+     // console.log("-----------------------------------------------------")
+     //console.log("CLICK!!!");
+     //console.log(move_x);
+     last_mouse_x = cursor_x;
+     last_mouse_y = cursor_y;
+   }
+   flag = !flag;
+ }
+
+ function problem(){
+   if(flag)
+    flag = !flag;
+ }
+
+ function update(){
+   point_x -= move_x;
+   point_y -= move_y;
+ }
+ function position (){
+   where_you_are = (parseInt(point_x / size_x ) + parseInt(point_y / size_y ) * column);
+   proportion_easy_x = size_x_false / size_x;
+   if (proportion_easy_x < 1){
+     depth = false;
+     election();
+     return;}
+ }
+
+ function calculate_neighbour(){
+   right   = parseInt (((point_x + ((max_carga_ancho/2) * size_x))/ size_x)+1) + parseInt(point_y / size_y ) * column;
+   left = parseInt ((point_x - ((max_carga_ancho/2) * size_x))/ size_x) + parseInt(point_y / size_y ) * column ;
+
+   up   = parseInt (point_x / size_x ) + parseInt((point_y - ((max_carga_alto/2) * size_y)) / size_y ) * column;
+   down = parseInt (point_x / size_x ) + parseInt(((point_y + ((max_carga_alto/2) * size_y)) / size_y )+1) * column;
+
+   while((((right % column * size_x) > reduc.width) || ((right % column) <= (where_you_are % column))) && (right != where_you_are))
+     right -= 1;
+
+   while (((left < 0) || ((left % column) >= (where_you_are % column))) && (left != where_you_are))
+     left += 1;
+
+   while ((down >= urls.length) && (down != where_you_are))
+     down -= column;
+
+   while ((up  < 0) && (up != where_you_are))
+     up += column;
+
+   for ( i = 0; i < urls.length; i++){
+     if ((left % column <= i % column) && ((right % column) >= (i % column)) && ( i >= (up - (where_you_are - left))) && ( i <= (down + (right - where_you_are)))){
+       index[index.length] = i;
+       if (img[i] == undefined){
+         img[i] = new Image();
+         img[i].src = urls[i];
+       }
      }
+   }
+ }
+ function calculate_display(){
+   relacion_x = size_x_img/size_x;
+   relacion_y = size_y_img/size_y;
 
-     /* Calcular el numero de column y filas que son necesarias
-     para cargar la imagen */
-     function maximum (){
-       max_carga_ancho = column;
-       max_carga_alto  = row;
-       if (size_x_img < canvas_destination_x)
-        max_carga_ancho = column + 1;
-       if (size_y_img < canvas_destination_y)
-        max_carga_alto = row + 1;
-     }
-
-     /*Genera un "table" con las url de las imágenes en las
-     que se descompone la imagen, el orden es de left
-     a right y de arriba a abajo */
-     function table (){
-       divide();
-       //aux = new Array(column);
-      for ( j = 0; j < column*row; j++)
-        urls[j] = "mariposa/" + j + ".jpeg";
-       }
-
-       /* Función general de llamada */
-       function mousepress(){
-         flag = !flag;
-       }
-       function mousemove (event){
-         if(flag){
-         index = [];
-         getMouseLomariposaionX(event); // Coordenadas en el canvas del ratón en x
-         getMouseLomariposaionY(event); // Coordenadas en el canvas del ratón en y
-         election();}
-       }
-       function election (){
-         if (depth){
-           position();               // Calcula en qué cuadrante del canvas se encuentra
-           calculate_neighbour();       // Calcula las imágenes necesarias para ampliar la zona y las carga
-           display();                // Dibujar la imagen final
-        }
-        else {
-          displayEasy();
-        }
-       }
-
-       /*Devuelve la coordenada X del ratón */
-       function getMouseLomariposaionX(event) {
-         var coordenadas = visor.getBoundingClientRect();
-         cursor_x = event.pageX - coordenadas.left;
-       }
-
-
-       /* Devuelve la coordenada Y del ratón */
-       function getMouseLomariposaionY(event){
-         var coordenadas = visor.getBoundingClientRect();
-         cursor_y = event.pageY - coordenadas.top;
-       }
-
-       /* Calcular posición actual */
-       function position (){
-         where_you_are = (parseInt(cursor_x / size_x ) + parseInt(cursor_y / size_y ) * column);
-         proportion_easy_x = size_x_false / size_x;
-         if (proportion_easy_x < 1){
-           depth = false;
-           election();
-           return;}
-       }
-
-       function calculate_neighbour(){
-         /* Lio  de paréntesis */ /* Solucionado */
-         right   = parseInt (((cursor_x + ((max_carga_ancho/2) * size_x))/ size_x)+1) + parseInt(cursor_y / size_y ) * column;
-         left = parseInt ((cursor_x - ((max_carga_ancho/2) * size_x))/ size_x) + parseInt(cursor_y / size_y ) * column ;
-
-         up   = parseInt (cursor_x / size_x ) + parseInt((cursor_y - ((max_carga_alto/2) * size_y)) / size_y ) * column;
-         down = parseInt (cursor_x / size_x ) + parseInt(((cursor_y + ((max_carga_alto/2) * size_y)) / size_y )+1) * column;
-
-         /* Solucionar: No siempre hay que entrar en el bucle */ /* Solucionado */
-         /* Solucionar: Problemas si right o left no está en la primera fila */
-         /* Solucionar: Las imágemes diagonales */
-
-         while((((right % column * size_x) > reduc.width) || ((right % column) <= (where_you_are % column))) && (right != where_you_are))
-           right -= 1;
-
-         while (((left < 0) || ((left % column) >= (where_you_are % column))) && (left != where_you_are))
-           left += 1;
-
-         while ((down >= urls.length) && (down != where_you_are))
-           down -= column;
-
-         while ((up  < 0) && (up != where_you_are))
-           up += column;
-
-
-         //Carga de imágenes
-         for ( i = 0; i < urls.length; i++){
-           if ((left % column <= i % column) && ((right % column) >= (i % column)) && ( i >= (up - (where_you_are - left))) && ( i <= (down + (right - where_you_are)))){
-             index[index.length] = i;
-             if (img[i] == undefined){
-               img[i] = new Image();
-               img[i].src = urls[i];
-             }
-           }
-         }
-       }       /* Fin function */
-
-       function display(){
-         var c = document.getElementById("visor");
-         var ctx = c.getContext("2d");
-
-         relacion_x = size_x_img / size_x;
-         relacion_y = size_y_img / size_y;
-
-         desplazamiento_x = (0.5 * canvas_destination_x) - cursor_x * relacion_x;
-         desplazamiento_y = (0.5 * canvas_destination_y) - cursor_y * relacion_y;
-
-
-         ctx.fillStyle = "#FFFFFF";
-         ctx.fillRect(0,0,size_x * column, size_y * row);
-
-         index.forEach(function(item){
-             dx = (((item % column ) * size_x  ) * relacion_x ) + desplazamiento_x;
-             dy = ((((item - (item % column )) / column ) * size_y ) * relacion_y) + desplazamiento_y;
-             ctx.drawImage(img[item],dx,dy,size_x_img,size_y_img);
-
-         })
-       }
-//var minimo = 0;
-//var contador = 3;
-       function zoom(alpha){
-         //if (alpha > 0  || contador > minimo){
-           index=[];
-           //var beta = 50;
-         //size_view_x -= alpha * 100;
-         //size_view_y -= alpha * 100 * proporcion;
-         size_x_false += alpha * 50 * size_x_img/size_y_img;
-         size_y_false += alpha * 50;
-
-         size_x_img = size_x_false;
-         size_y_img = size_y_false;
-
-         //contador += alpha;
-       //}
-         maximum();
-         election();
-       }
-
-      function Scroll(){
-      var situacion = document.getElementById("visor");
-      if (situacion.addEventListener){
-        //IE9, Chrome, Safari, Opera
-        situacion.addEventListener("mousewheel", MouseWheelHandler, false);
-        //Firefox
-        situacion.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
-      }
-      //IE 6/7/8
-      else situacion.attachEvent("onmousewheel", MouseWheelHandler);
+   desplazamiento_x += move_x;
+   desplazamiento_y += move_y;
+   if (((index[0] % column ) * size_x  ) + desplazamiento_x > 0){
+      desplazamiento_x = 0;
+      point_x = last_point[0];
     }
-      function MouseWheelHandler(e){
-        var e = window.event || e; //IE
-        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-        zoom(delta);
-        return false;
-      }
 
-      function displayEasy(){
-        var c = document.getElementById("visor");
-        var ctx = c.getContext("2d");
+  if(((((index[0] - (index[0] % column )) / column ) * size_y ) * relacion_y) + desplazamiento_y > 0){
+    desplazamiento_y = 0;
+    point_y = last_point[1];
+  }/*
+  console.log(row, size_x, desplazamiento_x,(row * size_x + desplazamiento_x ), canvas_destination_x, size_x == size_x_img)
+  if ((row * size_x_img + desplazamiento_x ) < (canvas_destination_x)){
+    //desplazamiento_x += +canvas_destination_x - (row * size_x_img + desplazamiento_x );
+    point_x = last_point[0];
+    console.log("jejeje");*/
+  }
 
-        var reduc = new Image();
-        reduc.src = "mariposa/reduc.jpeg";
+ }
 
-        proportion_easy_x = size_x_false / size_x;
-        if (proportion_easy_x > 1){
-          depth = true;
-          election();
-          return;}
-        var proporcion_easy_y = size_y_false / size_y;
+ function display(){
+   var c = document.getElementById("visor");
+   var ctx = c.getContext("2d");
 
-        desplazamiento_x =  -cursor_x * proportion_easy_x + (0.5 * canvas_destination_x);
-        desplazamiento_y =  -cursor_y * proporcion_easy_y + (0.5 * canvas_destination_y) ;
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect(0,0,size_x * column, size_y * row);
+   ctx.fillStyle = "#FFFFFF";
+   ctx.fillRect(0, 0, size_x * column, size_y * row);
 
-        ctx.drawImage(reduc, cursor_x-size_x_false*column/2, cursor_y-size_y_false*row/2, size_x_false*column, size_y_false*row);
-      }
+   index.forEach(function(item){
+       dx = (((item % column ) * size_x  ) * relacion_x ) + desplazamiento_x;
+       dy = ((((item - (item % column )) / column ) * size_y ) * relacion_y) + desplazamiento_y;
+       ctx.drawImage(img[item],dx,dy,size_x_img,size_y_img);
+   })
+ }
+
+ function mousemove (event){
+   if(flag){
+     index = [];
+     getMouseLocationX(event);
+     getMouseLocationY(event);
+     rest();
+     update();
+     election();
+   }
+ }
+
+ function election (){
+   if (depth){
+     if (one)
+      first();
+
+     else{
+       //rest();
+       //console.log("move:" + move_x + "  " + move_y);
+       //update();
+       //console.log("desplazado:" + point_x + "  " + point_y);
+       position();
+       calculate_neighbour();
+       calculate_display();
+       display();
+       save();
+       /*
+       if(move_x > 5 || move_y > 5 || move_y < -5 || move_x < -5)
+        console.log("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRR")*/
+     }
+  }
+  else {
+    calculate_display();
+    displayEasy();
+  }
+ }
